7c5c26ad91c9b84b16f2d869eb63f0e73f2e7507
Paul <paul.ryan.iii@gmail.com>
2018-05-08 08:10:49 -0700

    Fix respawning ship :)

--- src/ship.js
+++ src/ship.js
@@ -3,7 +3,6 @@ import MovingObject from './moving_object.js';
 const DEFAULTS = {
   COLOR: "#251351",
   RADIUS: 10,
-  SPEED: 0,
   VELOCITY: [0, 0]
 };
 
@@ -12,13 +11,12 @@ class Ship extends MovingObject {
     super(options);
     this.color = DEFAULTS.COLOR;
     this.radius = DEFAULTS.RADIUS;
-    this.speed = DEFAULTS.SPEED;
     this.vel = DEFAULTS.VELOCITY;
   }
 
   respawn() {
     this.pos = this.game.center();
-    this.vel = 0;
+    this.vel = [0, 0];
   }
 }
 
